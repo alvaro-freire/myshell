60bf01e9f708a0084d846a679e5495454fa5d956
alvaro-freire <alvaro.freirea@udc.es>
2021-12-09 17:44:12 +0100

    added header

--- signals.h
+++ signals.h
@@ -1,6 +1,11 @@
-//
-// Created by mateo on 09/12/2021.
-//
+/**
+ * TITLE: Operative Systems
+ * SUBTITLE: Lab Assignment 3
+ * @author Mateo Díaz Allegue @login mateo.diaz
+ * @author Álvaro Freire Ares @login alvaro.freirea
+ * GROUP: 4.2
+ * DATE: 4 / 12 / 2021
+ */
 
 #ifndef P3_SO_SIGNALS_H
 #define P3_SO_SIGNALS_H
