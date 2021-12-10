a0010ff72d3d85bd38cbe72ffb07daad43193fef
alvaro-freire <alvaro.freirea@udc.es>
2021-12-10 22:58:10 +0100

    deleted signals

--- signals.c
+++
@@ -1,9 +0,0 @@
-/**
- * TITLE: Operative Systems
- * SUBTITLE: Lab Assignment 3
- * @author Mateo Díaz Allegue @login mateo.diaz
- * @author Álvaro Freire Ares @login alvaro.freirea
- * GROUP: 4.2
- * DATE: 4 / 12 / 2021
- */
-
